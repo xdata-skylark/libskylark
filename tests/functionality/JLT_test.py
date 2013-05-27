example of averaging norms, still many parameters that need to be fixed

tests/functionality/JLT_test.py
@@ -1,48 +1,71 @@
 import unittest
+import itertools
+import random
 import numpy as np
-from mpi4py import MPI
 
-from helper.elemental_matrix import create_elemental_matrix
+from mpi4py import MPI
 from skylark import cskylark
+from helper.elemental_matrix import create_elemental_matrix
 
 import elem
 
 class JLT_test(unittest.TestCase):
 
     def setUp(self):
-        # Create a matrix to sketch
         elem.Initialize()
         self.A = create_elemental_matrix(10, 5)
 
-        # Initilize context
-        self.ctxt = cskylark.Context(123834)
-
-        #FIXME:
-        self.exp_col_norm = 17.2897784754
-        self.exp_row_norm = 42.9459176386
+        #FIXME: expected norm = ?
+        self.exp_col_norm = 28.49319221587624
+        self.exp_row_norm = 28.10891868832163
+        #FIXME: how many applications/averages
+        self.num_repeats  = 250
+        #FIXME: accuracy
+        self.num_places   = 5
 
     def tearDown(self):
         self.A.Free()
-        self.ctxt.Free()
         elem.Finalize()
 
-    #FIXME: how many applications/averages
     def test_apply_colwise(self):
-        S  = cskylark.JLT(self.ctxt, "DistMatrix_VR_STAR", "Matrix", 10, 6)
-        SA = elem.Mat()
-        SA.Resize(6, 5)
-        S.Apply(self.A, SA, 1)
 
-        self.assertAlmostEqual(np.linalg.norm(SA.Data()), self.exp_col_norm, places=5)
+        norm = 0.0
+        #for i in range(self.num_repeats):
+        for _ in itertools.repeat(None, self.num_repeats):
 
-    #FIXME: how many applications/averages
+            #FIXME: how to choose seeds?
+            ctxt = cskylark.Context(random.randint(0, 1e9))
+            S    = cskylark.JLT(ctxt, "DistMatrix_VR_STAR", "Matrix", 10, 6)
+            SA   = elem.Mat()
+            SA.Resize(6, 5)
+            S.Apply(self.A, SA, 1)
+
+            norm += np.linalg.norm(SA.Data())
+
+
+        norm /= self.num_repeats
+
+        self.assertAlmostEqual(norm, self.exp_col_norm, places=self.num_places)
+        ctxt.Free()
+
     def test_apply_rowwise(self):
-        S  = cskylark.JLT(self.ctxt, "DistMatrix_VR_STAR", "Matrix", 5, 3)
-        SA = elem.Mat()
-        SA.Resize(10, 3)
-        S.Apply(self.A, SA, 2)
 
-        self.assertAlmostEqual(np.linalg.norm(SA.Data()), self.exp_row_norm, places=5)
+        norm = 0.0
+        for i in range(self.num_repeats):
+
+            #FIXME: how to choose seeds?
+            ctxt = cskylark.Context(i)
+            S    = cskylark.JLT(ctxt, "DistMatrix_VR_STAR", "Matrix", 5, 3)
+            SA   = elem.Mat()
+            SA.Resize(10, 3)
+            S.Apply(self.A, SA, 2)
+
+            norm += np.linalg.norm(SA.Data())
+
+        norm /= self.num_repeats
+
+        self.assertAlmostEqual(norm, self.exp_row_norm, places=self.num_places)
+        ctxt.Free()
 
 
 if __name__ == '__main__':
f2aae69f62f941005d85f4d4c1670a5bcab08b62
Vikas Sindhwani <vsindhw@us.ibm.com>
2013-06-01 15:44:43 -0400

    Committing an SVD (HMT/Subspace iteration style)

---
+++ examples/svd.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <functional>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <ext/hash_map>
+
+#include <elemental.hpp>
+#include <boost/mpi.hpp>
+#include <skylark.hpp>
+
+#include "utilities.hpp"
+#include "parser.hpp"
+
+/*******************************************/
+namespace mpi  =  boost::mpi;
+namespace skys =  skylark::sketch;
+/*******************************************/
+
+/* These were declared as extern in utilities.hpp --- defining it here */
+int int_params [NUM_INT_PARAMETERS];
+char* chr_params[NUM_CHR_PARAMETERS];
+
+/** Typedef DistMatrix and Matrix */
+typedef std::vector<int> IntContainer;
+typedef std::vector<double> DblContainer;
+typedef elem::Matrix<double> MatrixType;
+typedef elem::DistMatrix<double, elem::VR, elem::STAR> DistMatrixType;
+
+int main (int argc, char** argv) {
+    /* Initialize MPI */
+    mpi::environment env (argc, argv);
+
+    /* Create a global communicator */
+    mpi::communicator world;
+
+    /* MPI sends argc and argv everywhere --- parse everywhere */
+    parse_parameters (argc,argv);
+
+    /* Initialize elemental */
+    elem::Initialize (argc, argv);
+    MPI_Comm mpi_world(world);
+    elem::Grid grid (mpi_world);
+
+    /* Initialize skylark */
+    skys::context_t context (int_params[RAND_SEED_INDEX], world);
+
+    int m = int_params[M_INDEX];
+    int n = int_params[N_INDEX];
+    int k = int_params[S_INDEX];
+
+    /* Create matrices A and B */
+    DistMatrixType A1(grid);
+    DistMatrixType A2(grid);
+
+    elem::DistMatrix<double> A11,A22,A33,V33;
+    DistMatrixType s33(grid);
+    DistMatrixType A(grid);
+    DistMatrixType U(grid);
+    //DistMatrixType U(m,k);
+    MatrixType s(k,1);
+    MatrixType V(n,k);
+
+
+    /** Only randomization is supported for now */
+    if (0==int_params[USE_RANDOM_INDEX]) {
+        /** TODO: Read the entries! */
+        std::cout << "We don't support reading --- yet --" << std::endl;
+    } else {
+        elem::Uniform (m, k, A1);
+        elem::Uniform (m, k, A2);
+        elem::Matrix<double> A3(k,k);
+      //  A1.Print("A1");
+      //  A2.Print("A2");
+
+        skylark::nla::Gemm(A1,A2,A3,context);
+        if (context.rank == 0) {
+  //      		A3.Print("Checking Matrix multiplication [VR,*]-times-[VR,*] A1'A2 = A3");
+        }
+
+        elem::Uniform (k,k, A3);
+
+//        A3.Print("Generated new A3:");
+        skylark::nla::Gemm(A1,A3,A2);
+
+    //    A2.Print("Checking Matrix multiplication between [VR,*] A1 and Matrix A3");
+
+        //A3.ResizeTo(m, n);
+        //V.ResizeTo(m, k);
+
+        elem::Uniform(m, k, A11);
+        elem::Uniform(k, n, A22);
+        A33.ResizeTo(m,n);
+        Gemm(elem::NORMAL, elem::NORMAL, 1.0, A11, A22, 0.0, A33);
+        A.ResizeTo(m,n);
+        A = A33;
+        elem::SVD(A33, s33, V33);
+        s33.Print("True singular values");
+
+        //A.ResizeTo(m, n);
+        //A = A3;
+      //  elem::Uniform (int_params[M_INDEX], int_params[N_RHS_INDEX], B);
+    }
+
+    /**
+     * Depending on which sketch is requested, do the sketching.
+     */
+    if (0==strcmp("JLT", chr_params[TRANSFORM_INDEX]) ) {
+
+        if (SKETCH_LEFT == int_params[SKETCH_DIRECTION_INDEX]) {
+        //	A.Print("Matrix A");
+        	//U.ResizeTo(m,k);
+            skylark::nla::SVD(A, U, s, V, int_params[S_INDEX], 1, context);
+        //	U.Print("U: Left Singular Vectors");
+        	s.Print("s: Singular Values");
+        //	V.Print("V: Right Singular Vectors");
+        } else {
+            //std::cout << "We only have left sketching. Please retry" << std::endl;
+
+
+
+        }
+    } else if (0==strcmp("FJLT", chr_params[TRANSFORM_INDEX]) ) {
+        if (SKETCH_LEFT == int_params[SKETCH_DIRECTION_INDEX]) {
+            /* 1. Create the sketching matrix */
+            skys::FJLT_t<DistMatrixType, MatrixType> FJLT (int_params[M_INDEX],
+                int_params[S_INDEX], context);
+
+            /* 2. Create space for the sketched matrix */
+            MatrixType sketch_A(int_params[S_INDEX], int_params[N_INDEX]);
+
+            /* 3. Apply the transform */
+            FJLT.apply (A, sketch_A, skys::columnwise_tag());
+
+            /* 4. Print and see the result */
+            if (int_params[S_INDEX] * int_params[M_INDEX] < 100 &&
+                world.rank() == 0)
+                sketch_A.Print ();
+
+            /** TODO: Do that same to B, and solve the system! */
+
+        }
+    } else if (0==strcmp("Sparse", chr_params[TRANSFORM_INDEX]) ) {
+        if (SKETCH_LEFT == int_params[SKETCH_DIRECTION_INDEX]) {
+
+            /* 1. Create the sketching matrix */
+            skys::CWT_t<DistMatrixType, MatrixType>
+                Sparse (int_params[M_INDEX], int_params[S_INDEX], context);
+
+            /* 2. Create space for the sketched matrix */
+            MatrixType sketch_A(int_params[S_INDEX], int_params[N_INDEX]);
+
+            /* 3. Apply the transform */
+            Sparse.apply (A, sketch_A, skys::columnwise_tag());
+
+            /* 4. Print and see the result */
+            if (int_params[S_INDEX] * int_params[M_INDEX] < 100 &&
+                world.rank() == 0)
+                sketch_A.Print ();
+
+            /** TODO: Do that same to B, and solve the system! */
+
+
+        }
+    } else {
+        std::cout << "We only have JLT/FJLT/Sparse sketching. Please retry" << 
+            std::endl;
+    }
+
+    elem::Finalize();
+
+    return 0;
+
+}
---
+++ nla/SVD_Elemental.hpp
@@ -0,0 +1,81 @@
+#ifndef SVD_ELEMENTAL_HPP
+#define SVD_ELEMENTAL_HPP
+
+#include <elemental.hpp>
+#include <skylark.hpp>
+
+
+typedef elem::Matrix<double> MatrixType;
+typedef elem::DistMatrix<double, elem::VR, elem::STAR> DistMatrixType;
+
+namespace skylark {
+namespace nla {
+
+// Takes an m x nA matrix A, m x nB matrix B and computes C = A'*B which is small nA x nB.
+// A and B are row-partitioned together. So computation of C boils down to C = sum_i A_i^T*B_i i.e. an mpi reduce operation.
+// Note: we need to pass the context so we can call mpi::reduce with the associated communicator.
+void Gemm(DistMatrixType& A, DistMatrixType& B, MatrixType& C, skylark::sketch::context_t& context) {
+
+	int mA = A.Height();
+	int nA = A.Width();
+	int mB = B.Height();
+	int nB = B.Width();
+
+	MatrixType C_local(nA, nB);
+	Gemm(elem::ADJOINT, elem::NORMAL, 1.0, A.LockedMatrix(), B.LockedMatrix(), 0.0, C_local);
+	boost::mpi::reduce (context.comm,
+	                            C_local.LockedBuffer(),
+	                            C_local.MemorySize(),
+	                            C.Buffer(),
+	                            std::plus<double>(),
+	                            0);
+}
+
+
+
+// Takes a m x n distributed matrix A and a n x l local matrix B and computes the distributed matrix A*B by local matrix multiplication.
+void Gemm(DistMatrixType& A, MatrixType& B, DistMatrixType& C) {
+	Gemm(elem::NORMAL, elem::NORMAL, 1.0, A.LockedMatrix(), B, 0.0, C.Matrix());
+}
+
+//templatize later
+void SVD(DistMatrixType& A, DistMatrixType& U, MatrixType& s,  MatrixType& V, int l, int q, skylark::sketch::context_t& context) {
+
+		int m = A.Height();
+		int n = A.Width();
+
+		// Create an n x l JLT Sketch
+		skylark::sketch::JLT_t<DistMatrixType, DistMatrixType> JLT (n, l, context);
+
+		// Create space to hold the sketched result
+		DistMatrixType Y(m,l);
+		//Y.ResizeTo(m,l);
+
+		JLT.apply (A, Y, skylark::sketch::rowwise_tag());
+
+		// TO DO : need to do power iterations here
+
+		// call Explicit QR on Y. Y is overwritten with Q where Y = QR.
+		// NOTE: Type conversions below.
+		elem::DistMatrix<double> Q(Y);
+		elem::ExplicitQR( Q );
+		DistMatrixType Q2(Q);
+		Q2 = Q;
+
+		//Compute B = Q'A of size l x n
+
+		MatrixType B(l, n);
+		Gemm(Q2, A, B, context);
+
+		// Get SVD of B - Note B is overwritten by U where B = U diag(s) V' is the SVD of B.
+	    elem::SVD(B, s, V);
+
+	    // Write U = Q B
+	    Gemm(Q2, B, U);
+	}
+
+	} //nla namespace
+} //skylark namespace
+
+#endif // SVD_HPP
+

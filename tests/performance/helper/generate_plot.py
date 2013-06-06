d4fdcec2f917b9b04cbe87dec3cd52ce567c325f
Yves Ineichen <yin@zurich.ibm.com>
2013-06-06 13:30:59 +0200

    improving plots and report

--- tests/performance/helper/generate_plot.py
+++ tests/performance/helper/generate_plot.py
@@ -4,6 +4,7 @@ import matplotlib
 # do not use any X backend
 matplotlib.use('Agg')
 import pylab
+import csv
 
 from datetime import date
 
@@ -12,10 +13,20 @@ def generate_plot(fname, output_dir):
     print("generating plot for %s" % (fname))
     name = fname[:fname.rfind('_')]
 
+    procs = []
+    with open(fname, 'r') as f:
+        reader = csv.reader(f, delimiter=' ')
+        for row in reader:
+            procs.append(int(row[0]))
+
+    procs = list(set(procs))
+
     pylab.plotfile(fname, cols=(0,1,2,3), delimiter=' ',
-            names=['$n_p$', 'min', 'avg', 'max'], subplots=False)
+            names=['$n_p$', 'min', 'avg', 'max'], subplots=False,
+            plotfuncs={1:'semilogy', 2:'semilogy', 3:'semilogy'})
     pylab.title("%s Performance" % (name))
     pylab.ylabel("time [s]")
+    pylab.xticks(procs)
     pylab.savefig("%s/%s.png" % (output_dir, fname))
     return "%s.png" % (fname)
 
@@ -27,7 +38,11 @@ def generate_plots(input_dir, output_dir):
     os.chdir(input_dir)
     for infile in glob.glob("*_test_*.perf"):
         png_file = generate_plot(infile, output_dir)
+        html += "<a href=\"%s\">\n" % (png_file)
         html += "<img src=\"%s\" width=\"450px\">\n" % (png_file)
+        html += "</a>\n"
+
+    html += "<hr>\n"
 
     with open(output_dir + "/latest.html", "w") as out:
         out.write(html)

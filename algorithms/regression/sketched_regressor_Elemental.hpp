a2d43e8d3b49aaf81e1dc9cd422444ccb98f3b30
Haim Avron <haimav@us.ibm.com>
2013-06-17 17:18:04 -0400

    Compute sketched solution only on rank 0 (for single node sketches).

--- algorithms/regression/sketched_regressor_Elemental.hpp
+++ algorithms/regression/sketched_regressor_Elemental.hpp
@@ -12,21 +12,24 @@ namespace skylark {
 namespace algorithms {
 
 /**
- * Sketched l2 regressor using sketch-and-solve.
+ * Sketched l2 regressor using sketch-and-solve when sketch fits into a 
+ * single node.
  *
- * This implementation assumes RhsType is the same as MatrixType.
+ * This implementation assumes RhsType is the same as MatrixType since
+ * we apply the same sketch to both the Matrix and Rhs.
  */
 template <typename MatrixType,
-          typename SketchType,
           template <typename, typename> class TransformType,
           typename ExactAlgTag>
 class sketched_regressor_t<l2_tag,
                            MatrixType,
                            MatrixType,
-                           SketchType,
+                           elem::Matrix<double>,
                            TransformType,
                            ExactAlgTag,
                            sketch_and_solve_tag> {
+
+    typedef elem::Matrix<double> SketchType;
 
     typedef regression_problem_t<l2_tag, MatrixType> problem_type;
     typedef regression_problem_t<l2_tag, SketchType> sketched_problem_type;
@@ -39,13 +42,15 @@ class sketched_regressor_t<l2_tag,
     typedef TransformType<MatrixType, SketchType> sketch_transform_type;
 
 private:
-    int _sketch_size;
+    const int _my_rank;
+    const int _sketch_size;
     sketch_transform_type *_sketch_transform;
     underlying_regressor_type  *_underlying_regressor;
 
 public:
     sketched_regressor_t(const problem_type &problem, int sketch_size,
-        sketch::context_t& context) : _sketch_size(sketch_size) {
+        sketch::context_t& context) :
+        _my_rank(context.rank), _sketch_size(sketch_size)  {
         // TODO m < n
         _sketch_transform =
             new sketch_transform_type(problem.m, sketch_size, context);
@@ -67,7 +72,8 @@ public:
         //      Want to make constructor independent.
         SketchType Sb(_sketch_size, 1);
         _sketch_transform->apply(b, Sb, sketch::columnwise_tag());
-        _underlying_regressor->solve(Sb, x);
+        if (_my_rank == 0)
+            _underlying_regressor->solve(Sb, x);
     }
 
     void solve_mulitple(const MatrixType &B, SketchType &X) {
@@ -75,7 +81,8 @@ public:
         //      Want to make constructor independent.
         SketchType SB(_sketch_size, B.Width());
         _sketch_transform->apply(SB, SB, sketch::columnwise_tag());
-        _underlying_regressor->solve_mulitple(SB, X);
+        if (_my_rank == 0)
+            _underlying_regressor->solve_mulitple(SB, X);
     }
 };
 

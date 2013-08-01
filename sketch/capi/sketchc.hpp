bf267b29fa2b4cc37304213ef1eb0d4a9a004594
Yves Ineichen <yin@zurich.ibm.com>
2013-08-01 12:20:28 +0200

    adding more doc

--- sketch/capi/sketchc.hpp
+++ sketch/capi/sketchc.hpp
@@ -31,7 +31,7 @@ enum matrix_type_t {
     MATRIX,                     /**< Dense elemental matrix */
     DIST_MATRIX_VC_STAR,
     DIST_MATRIX_VR_STAR,
-    DIST_SPARSE_MATRIX
+    DIST_SPARSE_MATRIX          /**< Sparse matrix (CombBLAS) */
 };
 
 struct sketch_transform_t {
@@ -59,20 +59,66 @@ namespace sketchc = skylark::sketch::c;
 
 extern "C" {
 
-/** Support for skylark::sketch::context_t. */
+// Support for skylark::sketch::context_t.
+
+/** Creating a default Skylark context required for applying sketches.
+ *  @param seed for the rng generator
+ *  @return a Skylark context
+ */
 SKYLARK_EXTERN_API sketch::context_t *sl_create_default_context(int seed);
+
+/** Creating a Skylark context required for applying sketches.
+ *  @param seed for the rng generator
+ *  @param cm MPI communicator used
+ *  @return a Skylark context
+ */
 SKYLARK_EXTERN_API sketch::context_t *sl_create_context(int seed, MPI_Comm cm);
+
+/** Free resources hold by a Skylark context.
+ *  @param ctxt Skylark context
+ */
 SKYLARK_EXTERN_API void sl_free_context(sketch::context_t *ctxt);
+
+/** Get rank.
+ *  @param ctxt Skylark context
+ *  @return MPI rank
+ */
 SKYLARK_EXTERN_API int sl_context_rank(sketch::context_t *ctxt);
+
+/** Get total number of processor.
+ *  @param ctxt Skylark context
+ *  @return number of processors in the context
+ */
 SKYLARK_EXTERN_API int sl_context_size(sketch::context_t *ctxt);
 
 
-/** Transforms */
+
+// Transforms
+/** Creating a sketch transformation.
+ *  @param ctxt Sklark context
+ *  @param type type of the sketch
+ *  @param input input matrix type
+ *  @param output output matrix type
+ *  @param n input size
+ *  @param s output size
+ *  @return sketch transformation
+ */
 SKYLARK_EXTERN_API sketchc::sketch_transform_t *sl_create_sketch_transform(
         sketch::context_t *ctxt, char *type, char *input, char *output,
         int n, int s, ...);
+
+/** Free resources hold by a sketch transformation.
+ *  @param S sketch transform
+ */
 SKYLARK_EXTERN_API void sl_free_sketch_transform(
         sketchc::sketch_transform_t *S);
+
+/** Apply the sketch transformation to a matrix.
+ *  @param S sketch transform
+ *  @param A input matrix
+ *  @param AS sketched matrix
+ *  @param dim direction of sketch application
+ */
 SKYLARK_EXTERN_API void sl_apply_sketch_transform(
         sketchc::sketch_transform_t *S, void *A, void *SA, int dim);
 

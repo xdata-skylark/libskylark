d9fb934c06b8b4c43f641d92c027d0cbe114066a
Yves Ineichen <yin@zurich.ibm.com>
2013-12-04 12:05:38 +0100

    collapsing regression test in one measurement test

--- tests/regression/svd_test.py
+++ tests/regression/svd_test.py
@@ -14,7 +14,7 @@ _M = 10000
 _N = 100
 _R = 1000
 
-class JLT_test(unittest.TestCase):
+class SVD_test(unittest.TestCase):
 
     def setUp(self):
         # We do not initialize Skylark so it will take the seed
@@ -28,12 +28,14 @@ class JLT_test(unittest.TestCase):
         self.accuracy    = 0.5
         _R = _N / self.accuracy**2
 
+        self.sketches = [cskylark.JLT, cskylark.FJLT, cskylark.CWT]
+
     def tearDown(self):
         # No real need to do this...
         #cskylark.finalize()
         pass
 
-    def check_result(self, results):
+    def check_result(self, results, sketch_name):
         suc = np.zeros(len(results[0].success))
         avg = np.zeros(len(results[0].average))
         for result in results:
@@ -41,11 +43,12 @@ class JLT_test(unittest.TestCase):
             suc = np.logical_or(suc, result.success)
 
         # check if at leaste one was successful
-        self.assertTrue(np.all(suc))
+        self.assertTrue(np.all(suc), msg=("Failed checking " + sketch_name))
 
         # check if average is in bounds
         avg = avg / self.num_repeats
-        self.assertTrue(np.all(avg <= self.accuracy))
+        self.assertTrue(np.all(avg <= self.accuracy),
+                        msg=("Failed checking " + sketch_name))
 
     def test_apply_colwise(self):
         A = elem.DistMatrix_d_VR_STAR()
@@ -53,10 +56,9 @@ class JLT_test(unittest.TestCase):
         #FIXME: Christos, use your matrix problem factory here
         elem.Uniform(A, _M, _N)
 
-        measures = [svd_bound] #.. add more measures to be computed in a test
-        results  = test_helper(A, _M, _N, _R, cskylark.JLT, measures, MPI)
-
-        self.check_result(results)
+        for sketch in self.sketches:
+            results = test_helper(A, _M, _N, _R, sketch, [svd_bound], MPI)
+            self.check_result(results, str(sketch))
 
     def test_apply_rowwise(self):
         A = elem.DistMatrix_d_VR_STAR()

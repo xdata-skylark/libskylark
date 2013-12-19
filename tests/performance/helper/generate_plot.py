70607bb8c6b3c89e704f836f6fc0b2ec0ca67689
Yves Ineichen <yin@zurich.ibm.com>
2013-12-19 15:05:26 +0100

    cleanup, doc

--- tests/performance/helper/generate_plot.py
+++ tests/performance/helper/generate_plot.py
@@ -28,7 +28,7 @@ def generate_plot_from_file(fname, web_dir):
     pylab.plotfile('tmp.dat', cols=(0,1,2,3), delimiter=' ',
             names=['$n_p$', 'min', 'avg', 'max'], subplots=False,
             plotfuncs={1:'semilogy', 2:'semilogy', 3:'semilogy'},
-            marker='s')
+            marker='s', linewidth=3)
     pylab.title("%s Performance" % (name))
     pylab.ylabel("time [s]")
     pylab.xticks(procs)
@@ -36,22 +36,24 @@ def generate_plot_from_file(fname, web_dir):
     return "%s.png" % (fname)
 
 
-import random
 def compute_efficiency(fname):
+    """
+    Currently just uses min and max processor results found in file to determine
+    the parallel efficiency.
+    """
     with open(fname, 'r') as f:
         reader = csv.reader(f, delimiter=' ')
         import operator
         sorted_data = sorted(reader, key=operator.itemgetter(0))
         r1 = sorted_data[0]
         rk = sorted_data[-1]
-        return (float(r1[2]) / float(rk[2])) / (int(rk[0])/float(r1[0]))
+        return (float(r1[2]) / float(rk[2])) / (int(rk[0]) / float(r1[0]))
 
 
 def update_overview(web_dir):
     """
     plot parallel efficiency of last month
     """
-
     import datetime
     today = datetime.date.today()
     dates = [ str(today - datetime.timedelta(days=x)) for x in range(30, -1, -1) ]
@@ -59,16 +61,17 @@ def update_overview(web_dir):
     perf = {}
     for date in dates:
         for infile in glob.glob("*_test_*_%s.perf" % date):
+            name = infile[:infile.rfind('_')]
             try:
-                perf[infile.split('_2013')[0]].append((date, compute_efficiency(infile)))
+                perf[name].append((date, compute_efficiency(infile)))
             except KeyError:
-                perf[infile.split('_2013')[0]] = [(date, compute_efficiency(infile))]
+                perf[name] = [(date, compute_efficiency(infile))]
 
     pylab.rcParams['figure.figsize'] = 20, 10
     actual_dates = []
     for name in perf:
         actual_dates, vals = zip(*perf[name])
-        pylab.plot(vals, hold=True, label="%s" % name)
+        pylab.plot(vals, hold=True, label="%s" % name, linewidth=3)
 
     pylab.xlabel("date")
     pylab.xticks(range(len(actual_dates)), actual_dates, rotation='vertical')
@@ -93,6 +96,7 @@ def generate_plots(input_dir, web_dir):
     #FIXME: collect results from other machines...
     html += "<h1 style=\"font-family: 'Voces';\">Performance on %s</h1>\n" % (date.today())
     os.chdir(input_dir)
+    # generate todays plots
     for infile in glob.glob("*_test_*_%s.perf" % date.today()):
         png_file = generate_plot_from_file(infile, web_dir)
         html += "<a href=\"plots/%s\">" % (png_file)
@@ -109,8 +113,9 @@ def generate_plots(input_dir, web_dir):
 
     archived = sorted(set(archived), reverse=True)
     html += "<ul style=\"font-family: 'Voces';\">\n"
+    #FIXME: generate pages in advance?
     for ar in archived:
-        html += "<li>%s</li>\n" % ar
+        html += "<li><a href=\"%s.html\">%s</a></li>\n" % (ar, ar)
 
     html += "</ul>\n"
     html += "</div></body></html>\n"

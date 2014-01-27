58a3f80cfe7219d30cad37c97355f1f5a5000f39
Haim Avron <haimav@us.ibm.com>
2014-01-27 10:25:33 -0500

    Adding sketched PCR

--- python-skylark/skylark/examples/example_krr.py
+++ python-skylark/skylark/examples/example_krr.py
@@ -28,6 +28,12 @@ model.train(X[1:trn,:], Y[1:trn], randomfeatures, regularization)
 predictions = model.predict(X[trn+1:,:])
 accuracy = skylark.metrics.classification_accuracy(predictions, Y[trn+1:])
 print "SketchedRLS Accuracy=%f%%" % accuracy
+
+model = sketchpcr(kernel)
+model.train(X[1:trn,:], Y[1:trn], randomfeatures)    
+predictions = model.predict(X[trn+1:,:])
+accuracy = skylark.metrics.classification_accuracy(predictions, Y[trn+1:])
+print "SketchedPCR Accuracy=%f%%" % accuracy
     
 model = nystromrls(kernel)
 model.train(X[1:trn,:], Y[1:trn], randomfeatures, regularization, probdist = 'uniform')
--- python-skylark/skylark/ml/nonlinear.py
+++ python-skylark/skylark/ml/nonlinear.py
@@ -2,6 +2,7 @@ import numpy, numpy.random, scipy.linalg, scipy.stats
 import utils
 from math import sqrt, cos, pi
 import skylark.io, skylark.metrics, skylark.sketch
+import skylark.nla.lowrank as lr
 import sys
 
 class rls(object):
@@ -148,7 +149,8 @@ class sketchrls(object):
     self.model = {}
     self._kernel = kernel				
 
-  def train(self, X, Y, random_features=100, regularization=1, multiclass=True):
+  def train(self, X, Y, random_features=100, regularization=1, 
+            multiclass=True, subtype=None):
     """
     Train the model.
     
@@ -166,13 +168,15 @@ class sketchrls(object):
 
     random_features: number of random features to use.
     
+    subtype: subtype for random features sketching.
+
     Returns
     --------
     Nothing. Internally sets the model parameters.
     
     """
 
-    self._rft = self._kernel.rft(random_features)
+    self._rft = self._kernel.rft(random_features, subtype)
     Z = self._rft / X
     
     I = numpy.identity(random_features)
@@ -272,6 +276,125 @@ class nystromrls(object):
     """
     Zt = self._kernel.gram(self.model["SX"], Xt)*self.model["U"]
     pred = Zt * self.model["weights"]
+    if self.model["multiclass"]:
+      pred = numpy.argmax(numpy.array(pred), axis=1)+1
+    return pred
+
+
+class sketchpcr(object):
+  """
+  Class for sketching based (aka random features) Non-linear Principal
+  Component Regression
+  
+  Example
+  -------
+
+  Read a digit classification dataset
+  
+  >>> X, Y = skylark.io.libsvm(sys.argv[1]).read()
+  
+  Set Regularization Parameter and Gaussian Kernel Bandwidth
+  
+  >>> bandwidth = 10.0
+  
+  Setup kernel:
+  
+  >>> import kernels
+  >>> kernel = kernels.gaussian(X.shape[1], bandwidth)
+  
+  Set number of random features
+  
+  >>> rank = 100
+  >>> s = 200
+  >>> t = 400
+	
+  Build a model on 1000 training examples
+  
+  >>> model = skylark.ml.nonlinear.sketchpcr(kernel)
+  >>> model.train(X[1:1000,:], Y[1:1000], rank)	
+  
+  Make Predictions on 1000 test examples
+  
+  >>> predictions = model.predict(X[1001:2000,:])
+  
+  Compute classification accuracy
+  
+  >>> accuracy = skylark.metrics.classification_accuracy(predictions, Y[1001:2000])
+  >>> print "SketchedPCR Accuracy=%f%%" % accuracy
+  SketchedRLS Accuracy=86.386386%
+  """
+  
+  def __init__(self, kernel):
+    self.model = {}
+    self._kernel = kernel				
+
+  def train(self, X, Y, rank, s=None, t=None, 
+            multiclass=True, subtype=None):
+    """
+    Train the model.
+    
+    
+    Parameters
+    ----------
+    X: m x n input matrix
+    
+    Y: m x 1 label vector (if multi-class classification problem, 
+       labels are from 0 to K-1 - trains one-vs-rest)
+    
+    rank: number of principal components to use.
+
+    s: First parameter for sketching. Defaults to rank * 2.
+    
+    t: Second parameter for sketching. Defaults to s * 2.
+
+    multiclass: is it a multiclass problem or not
+
+    subtype: subtype for kernel sketching.
+    
+    Returns
+    --------
+    Nothing. Internally sets the model parameters.
+    
+    """
+
+    if s is None:
+      s = 2 * rank
+    if t is None:
+      t = 2 * s
+
+    Z, S, R, V = lr.approximate_domsubspace_basis(X, rank, s, t, 
+                                                  self._kernel, subtype)
+        
+    if multiclass:
+      Y= utils.dummycoding(Y)
+      Y = 2*Y - 1
+
+    weights0 = numpy.dot(Z.T, Y)      
+    weights = scipy.linalg.solve_triangular(R, numpy.dot(V, weights0), lower=False)
+    
+    self._rft = S
+    self.model = {"kernel": self._kernel,
+                  "rft": self._rft,
+                  "weights": weights, 
+                  "s": s,
+                  "t": t,
+                  "rank": rank,
+                  "multiclass":multiclass}
+      
+  def predict(self, Xt):
+    """
+    Make predictions on test data
+    
+    Parameters
+    ----------
+    Xt: m x n input test matrix
+    
+    Returns
+    -------
+    m x 1 array of predictions on the test set.
+    """
+    Zt = self._rft / Xt
+    pred = numpy.dot(Zt, self.model["weights"])
     if self.model["multiclass"]:
       pred = numpy.argmax(numpy.array(pred), axis=1)+1
     return pred

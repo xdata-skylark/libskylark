72c0c244df41ed5b347d29cb15f07a1cc683e833
Haim Avron <haimav@us.ibm.com>
2014-02-04 14:35:24 -0500

    predict edits

--- python-skylark/skylark/ml/admm/predict.py
+++ python-skylark/skylark/ml/admm/predict.py
@@ -14,19 +14,32 @@ parser.add_argument("--testfile", type=str, help='Test dataset (file in libsvm f
 parser.add_argument("--modelfile", type=str, help='Save model in filename', required=True)
 parser.add_argument("--outputfile", type=str, help='Save predictions in filename', required=True)
 
-
 args = parser.parse_args()
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 NumProcessors = comm.Get_size()
 
+# load the model (as a dictionary)
+f = open(args.modelfile,'rb')
+model = cPickle.load(f)
+f.close()
+
+# Instantiate the model with the loaded dictionary
+model = KernelMachine(**model)
+
 # Load the data
 objective = 0
 if rank == 0:
     print "Parsing the data..."
-    data = skylark.io.sparselibsvm2scipy(args.testfile)
-    x,y = data
+    data = skylark.io.libsvm(args.testfile).read()
+
+    # If missing features, then augment the data
+    if data[0].shape[1] < model.RFTs[0].getindim():
+        fulldim = model.RFTs[0].getindim()
+        n = data[0].shape[0]
+        partialdim = data[0].shape[1]
+        data[0] = numpy.concatenate((data[0], numpy.zeros((n, fulldim - partialdim))))
 
 shape_X = data[0].shape if rank == 0 else None
 shape_X = comm.bcast(shape_X, root=0)
@@ -46,19 +59,11 @@ elem.Copy(X_cc, X);
 Y = elem.DistMatrix_d_VC_STAR()
 elem.Copy(Y_cc, Y);
 
-# load the model (as a dictionary)
-f = open(args.modelfile,'rb')
-model = cPickle.load(f)
-f.close()
-
 # predict with the model
-# Instantiate the model with the loaded dictionary
-model = KernelMachine(**model)
 predictions, labels = model.predict(X.Matrix)
 
 # need distributed accuracy computation
 accuracy = skylark.metrics.classification_accuracy(labels, y[:,0])
-
 
 print accuracy
 

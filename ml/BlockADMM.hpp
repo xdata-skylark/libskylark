18902702c09cdbba6953fdd8f0ad0552fab11ccc
Haim Avron <haimav@us.ibm.com>
2014-02-11 15:52:07 -0500

    Running and predicting good.

--- ml/BlockADMM.hpp
+++ ml/BlockADMM.hpp
@@ -78,6 +78,7 @@ public:
 	int train(DistInputMatrixType& X, DistTargetMatrixType& Y, LocalMatrixType& W);
 	void predict(DistInputMatrixType& X, DistTargetMatrixType& Y,LocalMatrixType& W);
 
+
 private:
 	skylark_context_t& context;
 	double lambda;
@@ -472,6 +473,7 @@ void BlockADMMSolver::predict(DistInputMatrixType& X, DistTargetMatrixType& Y, L
 		
 		elem::Matrix<double> o(ni, k);
 		
+
 		elem::View(Wslice, W, start, 0, sj, k);
 		elem::Gemm(elem::NORMAL, elem::NORMAL, 1.0, z, Wslice, 0.0, o);
 		elem::Axpy(+1.0, o, Y.Matrix());

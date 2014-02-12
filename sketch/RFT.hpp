414f583765c758bea3ce7cbe00b395e81c5b4be1
Haim Avron <haimav@us.ibm.com>
2014-02-12 16:29:41 -0500

    Added base class to GaussianRFT

--- sketch/RFT.hpp
+++ sketch/RFT.hpp
@@ -33,7 +33,8 @@ template< typename InputMatrixType,
 struct GaussianRFT_t :
     public GaussianRFT_data_t<typename
       RFT_t<InputMatrixType, OutputMatrixType,
-            bstrand::normal_distribution >::value_type > {
+            bstrand::normal_distribution >::value_type >,
+    virtual public sketch_transform_t<InputMatrixType, OutputMatrixType > {
 
 
     // We use composition to defer calls to RFT_t
@@ -73,14 +74,27 @@ struct GaussianRFT_t :
     }
 
     /**
-     * Apply the sketching transform that is described in by the sketch_of_A.
+     * Apply columnwise the sketching transform that is described by the
+     * the transform with output sketch_of_A.
      */
-    template <typename Dimension>
     void apply (const typename transform_t::matrix_type& A,
                 typename transform_t::output_matrix_type& sketch_of_A,
-                Dimension dimension) const {
+                columnwise_tag dimension) const {
         _transform.apply(A, sketch_of_A, dimension);
     }
+
+    /**
+     * Apply rowwise the sketching transform that is described by the
+     * the transform with output sketch_of_A.
+     */
+    void apply (const typename transform_t::matrix_type& A,
+                typename transform_t::output_matrix_type& sketch_of_A,
+                rowwise_tag dimension) const {
+        _transform.apply(A, sketch_of_A, dimension);
+    }
+
+    int get_N() const { return this->N; } /**< Get input dimesion. */
+    int get_S() const { return this->S; } /**< Get output dimesion. */
 
 private:
     transform_t _transform;

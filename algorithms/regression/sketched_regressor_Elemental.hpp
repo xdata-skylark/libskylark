0981e0f688ffdc4606bdb9679ab97090a6268436
Haim Avron <haimav@us.ibm.com>
2014-03-31 15:15:42 -0400

    Using typer

--- algorithms/regression/sketched_regressor_Elemental.hpp
+++ algorithms/regression/sketched_regressor_Elemental.hpp
@@ -1,11 +1,11 @@
-#ifndef SKETCHED_REGRESSOR_ELEMENTAL_HPP
-#define SKETCHED_REGRESSOR_ELEMENTAL_HPP
+#ifndef SKYLARK_SKETCHED_REGRESSOR_ELEMENTAL_HPP
+#define SKYLARK_SKETCHED_REGRESSOR_ELEMENTAL_HPP
 
 #include <elemental.hpp>
 
 #include "regression_problem.hpp"
 #include "../../sketch/sketch.hpp"
-
+#include "utility/typer.hpp"
 
 namespace skylark {
 namespace algorithms {
@@ -14,29 +14,32 @@ namespace algorithms {
  * Sketched generic regressor using sketch-and-solve when sketch fits into a
  * single node.
  *
- * This implementation assumes RhsType is the same as MatrixType since
+ * This implementation assumes RhsType is the same as InputType since
  * we apply the same sketch to both the Matrix and Rhs.
  */
 template <
     typename RegressionType,
-    typename ValueType,
-    typename MatrixType,
+    typename InputType,
     template <typename, typename> class TransformType,
     typename ExactAlgTag>
 class sketched_regressor_t<RegressionType,
-                           MatrixType,
-                           MatrixType,
-                           elem::Matrix<ValueType>,
+                           InputType,
+                           InputType,
+                           elem::Matrix<
+                               typename utility::typer_t<InputType>::value_type >,
                            TransformType,
                            ExactAlgTag,
                            sketch_and_solve_tag> {
 
-    typedef elem::Matrix<ValueType> matrix_type;
-    typedef elem::Matrix<ValueType> sketch_type;
+    typedef typename utility::typer_t<InputType>::value_type value_type;
+
+    typedef InputType matrix_type;
+    typedef elem::Matrix<value_type> sketch_type;
 
     typedef RegressionType regression_type;
 
-    typedef regression_problem_t<regression_type, MatrixType> problem_type;
+    typedef regression_problem_t<regression_type,
+                                 matrix_type> problem_type;
     typedef regression_problem_t<regression_type,
                                  sketch_type> sketched_problem_type;
 
@@ -45,7 +48,7 @@ class sketched_regressor_t<RegressionType,
                               sketch_type,
                               ExactAlgTag> underlying_regressor_type;
 
-    typedef TransformType<MatrixType, sketch_type> sketch_transform_type;
+    typedef TransformType<matrix_type, sketch_type> sketch_transform_type;
 
 private:
     const int _my_rank;
@@ -54,7 +57,7 @@ private:
     underlying_regressor_type  *_underlying_regressor;
 
 public:
-    sketched_regressor_t(const problem_type &problem, int sketch_size,
+    sketched_regressor_t(const problem_type& problem, int sketch_size,
         sketch::context_t& context) :
         _my_rank(context.rank), _sketch_size(sketch_size)  {
         // TODO m < n
@@ -73,14 +76,14 @@ public:
         delete _sketch_transform;
     }
 
-    void solve(const MatrixType &b, sketch_type &x) {
+    void solve(const matrix_type& b, sketch_type& x) {
         sketch_type Sb(_sketch_size, 1);
         _sketch_transform->apply(b, Sb, sketch::columnwise_tag());
         if (_my_rank == 0)
             _underlying_regressor->solve(Sb, x);
     }
 
-    void solve_mulitple(const MatrixType &B, sketch_type &X) {
+    void solve_mulitple(const matrix_type& B, sketch_type& X) {
         sketch_type SB(_sketch_size, B.Width());
         _sketch_transform->apply(SB, SB, sketch::columnwise_tag());
         if (_my_rank == 0)
@@ -110,6 +113,8 @@ class sketched_regressor_t<RegressionType,
                            TransformType,
                            ExactAlgTag,
                            sketch_and_solve_tag> {
+
+    typedef VT value_type;
 
     typedef elem::DistMatrix<VT, CD, RD> matrix_type;
     typedef elem::DistMatrix<VT, CD, RD> sketch_type;
@@ -153,7 +158,7 @@ public:
         delete _sketch_transform;
     }
 
-    void solve(const matrix_type &b, sketch_type &x) {
+    void solve(const matrix_type& b, sketch_type& x) {
         // TODO For DistMatrix this will allocate on DefaultGrid
         //      MIGHT BE VERY WRONG (grid is different).
         sketch_type Sb(_sketch_size, 1);
@@ -161,7 +166,7 @@ public:
         _underlying_regressor->solve(Sb, x);
     }
 
-    void solve_mulitple(const matrix_type &B, sketch_type &X) {
+    void solve_mulitple(const matrix_type& B, sketch_type& X) {
         // TODO For DistMatrix this will allocate on DefaultGrid...
         //      MIGHT BE VERY WRONG (grid is different).
         sketch_type SB(_sketch_size, B.Width());
@@ -170,6 +175,6 @@ public:
     }
 };
 
-} // namespace algorithms
-} // namespace skylark
-#endif // SKETCHED_REGRESSOR_ELEMENTAL_HPP
+} } // namespace skylark::algorithms
+
+#endif // SKYLARK_SKETCHED_REGRESSOR_ELEMENTAL_HPP

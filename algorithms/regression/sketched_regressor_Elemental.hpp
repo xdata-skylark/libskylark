446047ead41770f703916bb1f23be348e433195e
Haim Avron <haimav@us.ibm.com>
2014-04-01 14:03:45 -0400

    Adjustments to distributed sketched solver

--- algorithms/regression/sketched_regressor_Elemental.hpp
+++ algorithms/regression/sketched_regressor_Elemental.hpp
@@ -11,7 +11,7 @@ namespace skylark {
 namespace algorithms {
 
 /**
- * Generic sketched egressor using sketch-and-solve when sketch fits into a
+ * Generic sketched regressor using sketch-and-solve when sketch fits into a
  * single node.
  */
 template <
@@ -92,36 +92,35 @@ public:
 };
 
 /**
- * Sketched generic regressor using sketch-and-solve when both input and
- * output are distributed.
- *
- * This implementation assumes RhsType is the same as MatrixType since
- * we apply the same sketch to both the Matrix and Rhs. We further
- * make the simplifying assumption that sketch distribution
- * is the same.
+ * Generic sketched regressor using sketch-and-solve when sketch is distributed.
  */
 template <
     typename RegressionType,
-    typename VT,
+    typename InputType,
+    typename RhsType,
     elem::Distribution CD, elem::Distribution RD,
     template <typename, typename> class TransformType,
     typename ExactAlgTag>
 class sketched_regressor_t<RegressionType,
-                           elem::DistMatrix<VT, CD, RD>,
-                           elem::DistMatrix<VT, CD, RD>,
-                           elem::DistMatrix<VT, CD, RD>,
+                           InputType,
+                           RhsType,
+                           elem::DistMatrix<
+                               typename utility::typer_t<InputType>::value_type,
+                               CD, RD >,
                            TransformType,
                            ExactAlgTag,
                            sketch_and_solve_tag> {
 
-    typedef VT value_type;
+    typedef typename utility::typer_t<InputType>::value_type value_type;
 
-    typedef elem::DistMatrix<VT, CD, RD> matrix_type;
-    typedef elem::DistMatrix<VT, CD, RD> sketch_type;
+    typedef InputType matrix_type;
+    typedef RhsType rhs_type;
+    typedef elem::DistMatrix<value_type, CD, RD> sketch_type;
 
     typedef RegressionType regression_type;
 
-    typedef regression_problem_t<regression_type, matrix_type> problem_type;
+    typedef regression_problem_t<regression_type,
+                                 matrix_type> problem_type;
     typedef regression_problem_t<regression_type,
                                  sketch_type> sketched_problem_type;
 
@@ -130,47 +129,47 @@ class sketched_regressor_t<RegressionType,
                               sketch_type,
                               ExactAlgTag> underlying_regressor_type;
 
-    typedef TransformType<matrix_type, sketch_type> sketch_transform_type;
-
 private:
-    const int _my_rank;
+    typedef typename TransformType<matrix_type, sketch_type>::data_type
+    transform_data_type;
+
     const int _sketch_size;
-    sketch_transform_type *_sketch_transform;
-    underlying_regressor_type  *_underlying_regressor;
+    const transform_data_type _sketch;
+    const underlying_regressor_type  *_underlying_regressor;
 
 public:
-    sketched_regressor_t(const problem_type &problem, int sketch_size,
+    sketched_regressor_t(const problem_type& problem, int sketch_size,
         sketch::context_t& context) :
-        _my_rank(context.rank), _sketch_size(sketch_size)  {
+        _sketch_size(sketch_size),
+        _sketch(sketch_size, problem.n, context) {
         // TODO m < n
-        _sketch_transform =
-            new sketch_transform_type(problem.m, sketch_size, context);
+        TransformType<matrix_type, sketch_type> S(_sketch);
         // TODO For DistMatrix this will allocate on DefaultGrid...
         sketch_type sketch(sketch_size, problem.n);
-        _sketch_transform->apply(problem.input_matrix, sketch,
-            sketch::columnwise_tag());
+        S.apply(problem.input_matrix, sketch, sketch::columnwise_tag());
         sketched_problem_type sketched_problem(sketch_size, problem.n, sketch);
         _underlying_regressor = new underlying_regressor_type(sketched_problem);
     }
 
     ~sketched_regressor_t() {
         delete _underlying_regressor;
-        delete _sketch_transform;
     }
 
     void solve(const matrix_type& b, sketch_type& x) {
         // TODO For DistMatrix this will allocate on DefaultGrid
         //      MIGHT BE VERY WRONG (grid is different).
+        TransformType<rhs_type, sketch_type> S(_sketch);
         sketch_type Sb(_sketch_size, 1);
-        _sketch_transform->apply(b, Sb, sketch::columnwise_tag());
+        S.apply(b, Sb, sketch::columnwise_tag());
         _underlying_regressor->solve(Sb, x);
     }
 
     void solve_mulitple(const matrix_type& B, sketch_type& X) {
         // TODO For DistMatrix this will allocate on DefaultGrid...
         //      MIGHT BE VERY WRONG (grid is different).
+        TransformType<rhs_type, sketch_type> S(_sketch);
         sketch_type SB(_sketch_size, B.Width());
-        _sketch_transform->apply(SB, SB, sketch::columnwise_tag());
+        S.apply(SB, SB, sketch::columnwise_tag());
         _underlying_regressor->solve_mulitple(SB, X);
     }
 };

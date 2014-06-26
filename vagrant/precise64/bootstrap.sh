f5affe92fe5a34a470fd5f30d44af8a2f4ce7b2e
Georgios Kollias <gkollias@us.ibm.com>
2014-06-26 00:05:41 -0400

    split installation into dependencies and skylark parts

--- vagrant/precise64/bootstrap.sh
+++ vagrant/precise64/bootstrap.sh
@@ -210,15 +210,3 @@ easy_install numpydoc
 apt-get install -y dvipng
 
 
-source /home/vagrant/.bashrc
-
-# To build skylark (with CombBLAS support):
-yes | git clone https://github.com/xdata-skylark/libskylark.git
-
-mkdir ${SKYLARK_BUILD_DIR}
-mkdir ${SKYLARK_INSTALL_DIR}
-cd ${SKYLARK_BUILD_DIR}
-CXX=mpicc cmake -DCMAKE_INSTALL_PREFIX=${SKYLARK_INSTALL_DIR} -DUSE_COMBBLAS=ON ${SKYLARK_SRC_DIR}
-make
-make install
-

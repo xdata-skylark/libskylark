827fb802568e9f756c14cac9d92f0c184e8a5e4b
Haim Avron <haimav@us.ibm.com>
2014-11-01 19:51:24 -0400

    Updates in setup.py

--- python-skylark/setup.py
+++ python-skylark/setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(name='skylark',
       version='0.1',
-      description='Python-Skylark: A Big Data Package',
-      author='Skylark Team',
+      description='libSkylark: Sketching-based Matrix Computations for Machine Learninge',
+      author='IBM Corporation, Reseach Division',
       author_email='vsindhw@us.ibm.com',
-      url='http://www.skylark.org',
+      url='http://xdata-skylark.github.io/libskylark/',
       packages=['skylark'],
       package_dir = {'': 'python'}
      )

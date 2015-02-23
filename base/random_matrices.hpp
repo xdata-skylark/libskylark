e35ca041d3e5923145f004eb569ec294cb33c4e1
Haim Avron <haimav@us.ibm.com>
2015-02-23 11:21:48 -0500

    Completing a case for andom matrices

--- base/random_matrices.hpp
+++ base/random_matrices.hpp
@@ -38,6 +38,39 @@ void RandomMatrix(El::Matrix<T> &A, El::Int m, El::Int n,
 }
 
 /**
+ * Generate random matrix using specificed distribution
+ * (i.i.d samples).
+ *
+ * Implementation for distributed matrices.
+ *
+ * \param A Output matrix
+ * \param m,n Number of rows and colunt
+ * \param dist Distribution object
+ * \param context Skylark context.
+ */
+template<typename T, El::Distribution CD, El::Distribution RD,
+         template<typename> class DistributionType>
+void RandomMatrix(El::DistMatrix<T, CD, RD> &A, El::Int m, El::Int n,
+    DistributionType<T> &dist, context_t &context) {
+
+    utility::random_samples_array_t< DistributionType<T> > entries =
+        context.allocate_random_samples_array(m * n, dist);
+
+    A.Resize(m, n);
+
+    size_t m0 = A.LocalHeight();
+    size_t n0 = A.LocalWidth();
+    T *data = A.Buffer();
+
+#ifdef SKYLARK_HAVE_OPENMP
+#pragma omp parallel for collapse(2)
+#endif
+    for(size_t j = 0; j < n0; j++)
+        for(size_t i = 0; i < m0; i++)
+            data[j * m + i] = entries[A.GlobalCol(j) * m + A.GlobalRow(i)];
+}
+
+/**
  * Generate random matrix with i.i.d standard Gaussian entries.
  *
  * \param A Output matrix.

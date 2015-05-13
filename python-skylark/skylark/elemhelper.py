7c1e427a47bfdbc8676a92592544216e40ec94ed
Yves Ineichen <yin@zurich.ibm.com>
2015-05-13 15:17:03 +0200

    fixing elemhelper

--- python-skylark/skylark/elemhelper.py
+++ python-skylark/skylark/elemhelper.py
@@ -1,28 +1,29 @@
-import elem
+import El
 import numpy
 
-def create_elemental_matrix(m, n, f, type=elem.DistMatrix_d):
+def create_elemental_matrix(m, n, f, type=El.DistMatrix):
     """
     Create an Elemental matrix by setting position (i, j) to f(i, j).
     """
-    Adist = type(m, n)
-    localHeight = Adist.LocalHeight
-    localWidth = Adist.LocalWidth
-    colShift = Adist.ColShift
-    rowShift = Adist.RowShift
-    colStride = Adist.ColStride
-    rowStride = Adist.RowStride
-    data = Adist.Matrix
-    ldim = Adist.LDim
+    Adist = type()
+    Adist.Resize(m, n)
+    localHeight = Adist.LocalHeight()
+    localWidth = Adist.LocalWidth()
+    colShift = Adist.ColShift()
+    rowShift = Adist.RowShift()
+    colStride = Adist.ColStride()
+    rowStride = Adist.RowStride()
+    data = Adist.Matrix()
+    ldim = Adist.LDim()
     for jLocal in xrange(0,localWidth):
         j = rowShift + jLocal*rowStride
         for iLocal in xrange(0,localHeight):
             i = colShift + iLocal*colStride
-            data[iLocal, jLocal] = f(i, j)
-    
+            Adist.SetLocal(iLocal, jLocal, f(i, j))
+
     return Adist
 
-def local2distributed(A, type=elem.DistMatrix_d):
+def local2distributed(A, type=El.DistMatrix):
     """
     Takes an NumPy local matrix and returns an Elemental distribute matrix.
     The type of the Elemental is given by type, whose default is DistMatrix_d.
@@ -37,19 +38,20 @@ def local2distributed(A, type=elem.DistMatrix_d):
     return Adist
 
 
-    Adist = tp(A.shape[0], A.shape[1])
-    localHeight = Adist.LocalHeight
-    localWidth = Adist.LocalWidth
-    colShift = Adist.ColShift
-    rowShift = Adist.RowShift
-    colStride = Adist.ColStride
-    rowStride = Adist.RowStride
-    data = Adist.Matrix
-    ldim = Adist.LDim
+    Adist = type()
+    Adist.Resize(A.shape[0], A.shape[1])
+    localHeight = Adist.LocalHeight()
+    localWidth = Adist.LocalWidth()
+    colShift = Adist.ColShift()
+    rowShift = Adist.RowShift()
+    colStride = Adist.ColStride()
+    rowStride = Adist.RowStride()
+    data = Adist.Matrix()
+    ldim = Adist.LDim()
     for jLocal in xrange(0,localWidth):
         j = rowShift + jLocal*rowStride
         for iLocal in xrange(0,localHeight):
             i = colShift + iLocal*colStride
-            data[iLocal, jLocal] = A[i, j]
-    
+            Adist.SetLocal(iLocal, jLocal, f(i, j))
+
     return Adist

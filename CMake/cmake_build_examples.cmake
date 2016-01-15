05017aa3310ca219ca7599b1d07370bcc0cfd875
Yves Ineichen <yin@zurich.ibm.com>
2016-01-15 12:21:06 +0100

    CMake: disable building examples and tests by default (requested by @haimav)

--- CMake/cmake_build_examples.cmake
+++ CMake/cmake_build_examples.cmake
@@ -1,4 +1,4 @@
-option (BUILD_EXAMPLES "Whether we should build the examples" ON)
+option (BUILD_EXAMPLES "Whether we should build the examples" OFF)
 if (BUILD_EXAMPLES)
   add_subdirectory(examples)
 endif (BUILD_EXAMPLES)

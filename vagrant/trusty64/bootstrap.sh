261c6bfa0e8eb6ea37659afe66db2bb2c0c7283f
Yves Ineichen <yin@zurich.ibm.com>
2016-02-25 09:18:56 +0100

    vagrant: export proper paths for notebook server to work

--- vagrant/trusty64/bootstrap.sh
+++ vagrant/trusty64/bootstrap.sh
@@ -309,7 +309,9 @@ cd /home/${UNAME}
 dtach -n /tmp/ipython_notebook -Ez ipython notebook --profile=nbserver
 
 #FIXME: dir to /home/vagrant, run as vagrant user?
-echo "LD_LIBRARY_PATH=/home/${UNAME}/install/lib:/usr/local/lib:$LD_LIBRARY_PATH LD_PRELOAD=/usr/lib/libmpi.so dtach -n /tmp/ipython_notebook -Ez ipython notebook --profile=nbserver" > /etc/rc.local
+echo "export LD_LIBRARY_PATH=/home/${UNAME}/install/lib:/usr/local/lib:$LD_LIBRARY_PATH" > /etc/rc.local
+echo "export LD_PRELOAD=/usr/lib/libmpi.so" > /etc/rc.local
+echo "dtach -n /tmp/ipython_notebook -Ez ipython notebook --profile=nbserver" > /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 
 echo "Started IPython notebook. Point your browser to http://127.0.0.1:6868"

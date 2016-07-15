75716990056afa0f72dc2e57f494b2c5f616d95b
Georgios Kollias <gkollias@us.ibm.com>
2016-07-15 19:05:58 -0400

    builds with CombBLAS ON in the conda recipe

--- conda/libskylark/build.sh
+++ conda/libskylark/build.sh
@@ -11,13 +11,14 @@ export BOOST_ROOT="${PREFIX}"
 export RANDOM123_ROOT="${PREFIX}"
 export ELEMENTAL_ROOT="${PREFIX}"
 export FFTW_ROOT="${PREFIX}"
+export COMBBLAS_ROOT="${PREFIX}"
 
 
 CC="${PREFIX}/bin/mpicc -cc=${PREFIX}/bin/gcc" CXX="${PREFIX}/bin/mpicxx -cxx=${PREFIX}/bin/g++" \
 cmake \
 -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 -DUSE_FFTW=ON \
--DUSE_COMBBLAS=OFF \
+-DUSE_COMBBLAS=ON \
 .
 
 make -j "${NPROC}"

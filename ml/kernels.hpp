169d56541d3e9e0a7831ee3c8058136883007753
Jomsdev <jomsdev@gmail.com>
2016-08-30 20:08:18 -0700

    kernels.hpp: expsemigroup_t is using the kernel_t interface now.
    The expsemigroup kernel has implemented the gram fucntions but not the symmetric_gram yet.

--- ml/kernels.hpp
+++ ml/kernels.hpp
@@ -916,7 +916,7 @@ private:
 };
 
 
-struct expsemigroup_t {
+struct expsemigroup_t : public kernel_t {
 
     expsemigroup_t(El::Int N, double beta) : _N(N), _beta(beta) {
 
@@ -932,6 +932,18 @@ struct expsemigroup_t {
         pt.put("N", _N);
 
         return pt;
+    }
+
+    sketch::sketch_transform_t<boost::any, boost::any> *create_rft(El::Int S,
+    regular_feature_transform_tag tag, base::context_t& context) const {
+
+        return create_rft<boost::any, boost::any>(S, tag, context);
+    }
+
+    sketch::sketch_transform_t<boost::any, boost::any> *create_rft(El::Int S,
+    fast_feature_transform_tag tag, base::context_t& context) const {
+        // TODO
+        return nullptr;
     }
 
     template<typename IT, typename OT>
@@ -960,7 +972,98 @@ struct expsemigroup_t {
             qmc_sequence_dim(_N);
     }
 
-    // TODO method for gram matrix ?
+    template<typename XT, typename YT, typename KT>
+    void gram(base::direction_t dirX, base::direction_t dirY,
+        const XT &X, const YT &Y, KT &K) const {
+
+        typedef typename utility::typer_t<KT>::value_type value_type;
+
+        El::Int m = dirX == base::COLUMNS ? base::Width(X) : base::Height(X);
+        El::Int n = dirY == base::COLUMNS ? base::Width(Y) : base::Height(Y);
+
+        K.Resize(m, n);
+        base::ExpsemigroupDistanceMatrix(dirX, dirY, value_type(1.0), X, Y,
+            value_type(0.0), K);
+        El::EntrywiseMap(K, std::function<value_type(value_type)> (
+            [this] (value_type x) {
+                return std::exp(-_beta * x);
+            }));
+    }
+
+    template<typename XT, typename KT>
+    void symmetric_gram(El::UpperOrLower uplo, base::direction_t dir,
+        const XT &X, KT &K) const {
+
+        // TODO: Implement the kernel for symmetric matrices
+       
+    }
+
+    /* Instantion of virtual functions in base */
+
+    void gram(base::direction_t dirX, base::direction_t dirY,
+        const El::Matrix<double> &X, const El::Matrix<double> &Y,
+        El::Matrix<double> &K) const {
+
+        typedef El::Matrix<double> matrix_type;
+        gram<matrix_type, matrix_type, matrix_type>(dirX, dirY, X, Y, K);
+    }
+
+    void gram(base::direction_t dirX, base::direction_t dirY,
+        const El::Matrix<float> &X, const El::Matrix<float> &Y,
+        El::Matrix<float> &K) const {
+
+        typedef El::Matrix<float> matrix_type;
+        gram<matrix_type, matrix_type, matrix_type>(dirX, dirY, X, Y, K);
+    }
+
+    void gram(base::direction_t dirX, base::direction_t dirY,
+        const El::ElementalMatrix<double> &X,
+        const El::ElementalMatrix<double> &Y,
+        El::ElementalMatrix<double> &K) const {
+
+        typedef El::ElementalMatrix<double> matrix_type;
+        gram<matrix_type, matrix_type, matrix_type>(dirX, dirY, X, Y, K);
+    }
+
+    void gram(base::direction_t dirX, base::direction_t dirY,
+        const El::ElementalMatrix<float> &X,
+        const El::ElementalMatrix<float> &Y,
+        El::ElementalMatrix<float> &K) const {
+
+        typedef El::ElementalMatrix<float> matrix_type;
+        gram<matrix_type, matrix_type, matrix_type>(dirX, dirY, X, Y, K);
+    }
+
+    virtual void symmetric_gram(El::UpperOrLower uplo, base::direction_t dir, 
+        const El::Matrix<double> &X, El::Matrix<double> &K) const {
+
+        typedef El::Matrix<double> matrix_type;
+        symmetric_gram<matrix_type, matrix_type>(uplo, dir, X, K);
+
+    }
+
+    virtual void symmetric_gram(El::UpperOrLower uplo, base::direction_t dir,
+        const El::Matrix<float> &X, El::Matrix<float> &K) const {
+
+        typedef El::Matrix<float> matrix_type;
+        symmetric_gram<matrix_type, matrix_type>(uplo, dir, X, K);
+    }
+
+    virtual void symmetric_gram(El::UpperOrLower uplo, base::direction_t dir,
+        const El::ElementalMatrix<double> &X,
+        El::ElementalMatrix<double> &K) const {
+
+        typedef El::ElementalMatrix<double> matrix_type;
+        symmetric_gram<matrix_type, matrix_type>(uplo, dir, X, K);
+    }
+
+    virtual void symmetric_gram(El::UpperOrLower uplo, base::direction_t dir,
+        const El::ElementalMatrix<float> &X,
+        El::ElementalMatrix<float> &K) const {
+
+        typedef El::ElementalMatrix<float> matrix_type;
+        symmetric_gram<matrix_type, matrix_type>(uplo, dir, X, K);
+    }
 
 
 private:

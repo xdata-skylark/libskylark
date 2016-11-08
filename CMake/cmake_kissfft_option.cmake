45f9f94fe885c6c1bd4ee17c235a607f57af623b
jms <jomsdev@gmail.com>
2016-11-08 12:09:01 -0800

    KISSFFT_SOURCE_FILES added.
    Added the cmake variable.
    nla and capi use kissfft by defoult now.

--- CMake/cmake_kissfft_option.cmake
+++ CMake/cmake_kissfft_option.cmake
@@ -1,9 +1,16 @@
 option (USE_KISSFFT "Build with KISSFFT support" OFF)
+SET(KISSFFT_SOURCE_FILES "")
 if (USE_KISSFFT)
   set (CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/CMakeModules")
   find_package(KISSFFT)
   if (KISSFFT_FOUND)
-    include_directories(${KISSFFT_INCLUDE_DIR})
+    include_directories(${KISSFFT_INCLUDE_DIR} ${KISSFFT_INCLUDE_TOOLS})
+
+    SET(KISSFFT_SOURCE_FILES
+        ${KISSFFT_INCLUDE_DIR}/kiss_fft.c
+        ${KISSFFT_INCLUDE_TOOLS}/kiss_fftr.c
+    )
+
     set (SKYLARK_HAVE_KISSFFT
          1
          CACHE

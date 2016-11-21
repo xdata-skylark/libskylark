1c875a205c91883801924448c419c59623b784ba
jms <jomsdev@gmail.com>
2016-11-21 10:52:17 -0800

    CMAKE KISSFFT_SOURCE_FILES removed
    We do not need it any more because whe use the .hh version (templated header file)

--- CMake/cmake_kissfft_option.cmake
+++ CMake/cmake_kissfft_option.cmake
@@ -1,14 +1,9 @@
 option (USE_KISSFFT "Build with KISSFFT support" OFF)
-SET(KISSFFT_SOURCE_FILES "")
 if (USE_KISSFFT)
   set (CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/CMakeModules")
   find_package(KISSFFT)
   if (KISSFFT_FOUND)
     include_directories(${KISSFFT_INCLUDE_DIR})
-
-    SET(KISSFFT_SOURCE_FILES
-        ${KISSFFT_INCLUDE_DIR}/kissfft.hh
-    )
 
     set (SKYLARK_HAVE_KISSFFT
          1

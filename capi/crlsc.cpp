5b2aa734e6366c497ddea5f29c8e21bee58c707b
jms <jomsdev@gmail.com>
2017-01-12 15:38:17 -0800

    sl_approximate_kernel_rlsc function exposed to CAPI

--- capi/crlsc.cpp
+++ capi/crlsc.cpp
@@ -26,7 +26,7 @@ SKYLARK_EXTERN_API int sl_kernel_rlsc(
     std::stringstream data;
     data << params_json;
     boost::property_tree::read_json(data, json_tree);
-    skylark::ml::rlsc_params_t params;
+    skylark::ml::rlsc_params_t params(data);
 
     auto *rcoding = new std::vector<El::Int>();
 
@@ -50,5 +50,55 @@ SKYLARK_EXTERN_API int sl_kernel_rlsc(
     delete rcoding;
     return 0;
 }
+
+
+SKYLARK_EXTERN_API int sl_approximate_kernel_rlsc(
+    int direction_, sl_kernel_t *k,
+    char *X_type, void *X_, char *L_type, void *L_,
+    double lambda, sketch::sketch_transform_container_t<El::DistMatrix<double>,
+        El::DistMatrix<double> > **S_,
+    char *W_type, void *W_, El::DistMatrix<El::Int> *rcoding_,
+    int s, sl_context_t *ctxt,
+    char *params_json) {
+
+    skylark::base::direction_t direction =
+        direction_ == SL_COLUMNS ? skylark::base::COLUMNS : skylark::base::ROWS;
+
+    boost::property_tree::ptree json_tree;
+    std::stringstream data;
+    data << params_json;
+    boost::property_tree::read_json(data, json_tree);
+    skylark::ml::rlsc_params_t params(data);
+    
+
+    auto *rcoding = new std::vector<El::Int>();
+    
+    *S_ = new sketch::sketch_transform_container_t<
+            El::DistMatrix<double>, El::DistMatrix<double> >();
+
+    SKYLARK_BEGIN_TRY()
+        skylark::ml::ApproximateKernelRLSC(direction,
+            k->kernel_obj, 
+            skylark_void2any(X_type, X_),
+            skylark_void2any(L_type, L_),
+            lambda,
+            **S_,
+            skylark_void2any(W_type, W_),
+            *rcoding,
+            El::Int(s),
+            dref_context(ctxt),
+            params);
+    SKYLARK_END_TRY()
+        SKYLARK_CATCH_COPY_AND_RETURN_ERROR_CODE(lastexception);
+
+    rcoding_->Resize(1, rcoding->size());
+    for (int i = 0; i < rcoding->size(); ++i) {
+        rcoding_->Set(0, i, rcoding->at(i));
+    }
+
+    delete rcoding;
+    return 0;
+}
+
 
 } // extern "C"
--- capi/rlscc.hpp
+++ capi/rlscc.hpp
@@ -2,6 +2,7 @@
 #define SKYLARK_RLSCC_HPP
 
 #include "kernelc.hpp" // Defines sl_kernel_t
+#include "sketchc.hpp" // Defines sl_sketch_transform_t
 
 extern "C" {
 
@@ -10,6 +11,15 @@ SKYLARK_EXTERN_API int sl_kernel_rlsc(
     char *X_type, void *X_, char *L_type, void *L_,
     double lambda,  char *A_type, void *A_,
     El::DistMatrix<El::Int> *rcoding_,
+    char *params_json);
+
+SKYLARK_EXTERN_API int sl_approximate_kernel_rlsc(
+    int direction_, sl_kernel_t *k,
+    char *X_type, void *X_, char *L_type, void *L_,
+    double lambda, sketch::sketch_transform_container_t<El::DistMatrix<double>, 
+        El::DistMatrix<double> > **S_,
+    char *W_type, void *W_, El::DistMatrix<El::Int> *rcoding_,
+    int s, sl_context_t *ctxt,
     char *params_json);
 }
 

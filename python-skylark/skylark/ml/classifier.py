ef39ae10504bcfd2c35962f1c03f175513509ea0
jms <jomsdev@gmail.com>
2017-01-26 10:24:47 -0800

    Classifier implementation updated

--- python-skylark/skylark/ml/classifier.py
+++ python-skylark/skylark/ml/classifier.py
@@ -8,23 +8,55 @@ import skylark.lib as lib
 from ctypes import byref, c_void_p, c_double, c_int
 
 class Classifier(object):
-  
-  def load_data(self, fname, dir="columns", min_d=0, left_type=El.dTag, right_type=El.iTag):
-    self.left_type = left_type
-    self.right_type = right_type
+
+  def train_krr(self, lambda_value):
+    (self.x, self.rcoding) = sl_rlsc.kernel_rlsc(self.A, self.b, self.x, \
+        self.kernel, lambda_value, dir=self.dir)
+
+  def predict_krr(self):
+    KT = El.DistMatrix()
+    self.kernel.gram(self.A, KT, self.dir, self.dir, self.A_test)
+    self.DV = El.DistMatrix()
+    self.DV.Resize(self.x.Width(), KT.Width())
+
+    El.Gemm(El.ADJOINT, El.NORMAL, 1.0, self.x, KT, 0.0, self.DV)
+
+
+  def train_approximate_krr(self, lambda_value, s):
+    (self.x, self.rcoding, self.S) = sl_rlsc.approximate_kernel_rlsc(self.A, self.b, \
+        self.x, self.kernel, lambda_value, s, dir=self.dir)
+     
+    SA = El.DistMatrix()
+    SA.Resize(s, self.A_test.Width())
+
+    Aux = lib.adapt(self.A_test)
+    SAux = lib.adapt(SA)
+
+    lib.callsl("sl_apply_sketch_transform_container", self.S, \
+      Aux.ptr(), SAux.ptr())
+    self.SA = SAux.getobj()
+    
+  def predict_approximate_krr(self):
+    self.DV = El.DistMatrix()
+    self.DV.Resize(self.x.Width(), self.SA.Width())
+
+    El.Gemm(El.ADJOINT, El.NORMAL, 1.0, self.x, self.SA, 0.0, self.DV)
+
+
+  def set_training_data(self, A, b, dir="columns"):
+    self.A = A
+    self.b = b
     self.dir = dir
-    self.min_d = min_d
 
 
-    self.A = El.DistMatrix(left_type)
-    self.b = El.DistMatrix(right_type)
-    sl_io.readlibsvm(fname, self.A, self.b, dir, min_d=min_d)
+  def set_test_data(self, A_test, b_test):
+    self.A_test = A_test
+    self.b_test = El.DistMatrix(El.iTag)
 
-
-  def load_test_data(self, fname, dir="columns"):
-    self.Atest = El.DistMatrix(self.left_type)
-    self.btest = El.DistMatrix(self.right_type)
-    sl_io.readlibsvm(fname, self.Atest, self.btest, self.dir, min_d=self.min_d)
+    if b_test.Height == 1:
+      self.b_test = b_test
+    else:
+      El.Transpose(b_test, self.b_test)
 
 
   def set_kernel(self, kernel, *args):
@@ -42,55 +74,36 @@ class Classifier(object):
     self.x = El.DistMatrix()
     
     if algorithm == "krr":
-      (self.x, self.rcoding) = sl_rlsc.kernel_rlsc(self.A, self.b, self.x, \
-        self.kernel, args[0], dir=self.dir)
+      self.train_krr(args[0])
     elif algorithm == "approximate_krr":
-      (self.x, self.rcoding, self.S) = sl_rlsc.approximate_kernel_rlsc(self.A, self.b, \
-        self.x, self.kernel, args[0], args[1], dir=self.dir)
-      # This code should not be here
-      SA = El.DistMatrix()
-      SA.Resize(args[1], self.Atest.Width())
-
-      Aux = lib.adapt(self.Atest)
-      SAux = lib.adapt(SA)
-
-      lib.callsl("sl_apply_sketch_transform_container", self.S, \
-        Aux.ptr(), SAux.ptr())
-      self.SA = SAux.getobj()
+      self.train_approximate_krr(args[0], args[1])
     else:
       raise ValueError(algorithm + " is not implemented")
   
 
+
   def predict(self):
     if self.algorithm == "krr":
-      KT = El.DistMatrix()
-      self.kernel.gram(self.A, KT, self.dir, self.dir, self.Atest)
-      self.DV = El.DistMatrix()
-      self.DV.Resize(self.x.Width(), KT.Width())
-
-      El.Gemm(El.ADJOINT, El.NORMAL, 1.0, self.x, KT, 0.0, self.DV)
+      self.predict_krr()
     elif self.algorithm == "approximate_krr":
-      self.DV = El.DistMatrix()
-      self.DV.Resize(self.x.Width(), self.SA.Width())
+      self.predict_approximate_krr()
 
-      El.Gemm(El.ADJOINT, El.NORMAL, 1.0, self.x, self.SA, 0.0, self.DV)
-    
-    self.predictions = self.dummyDecode()
+    self.predictions = self.dummyDecode()    
     return self.predictions
 
 
   def error_rate(self):      
     errors = 0.0
+    n = self.b_test.Height()
 
-    
-    Aux = El.DistMatrix(self.right_type)
-    El.Transpose(self.btest, Aux)
-    n = Aux.Height()
     for i in xrange(n):
-      if Aux.Get(i, 0) != self.predictions.Get(i, 0):
+      if self.b_test.Get(i, 0) != self.predictions.Get(i, 0):
         errors += 1
 
-    return errors/n*100
+    return errors/n
+
+  def accuracy(self):      
+    return 1-error_rate
 
   def dummyDecode(self):
     predictions = El.DistMatrix()
